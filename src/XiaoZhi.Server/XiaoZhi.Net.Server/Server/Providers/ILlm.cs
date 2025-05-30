1b216147090e44bdae087ff40eb6389105c7e856
mm7h <sun7key@gmail.com>
2025-05-30 22:22:47 +0800

    整理代码结构

--- src/XiaoZhi.Server/XiaoZhi.Net.Server/Server/Providers/ILlm.cs
+++ src/XiaoZhi.Server/XiaoZhi.Net.Server/Server/Providers/ILlm.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
-using XiaoZhi.Net.Server.Common.Entities;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 using XiaoZhi.Net.Server.Common.Contexts;
+using XiaoZhi.Net.Server.Common.Entities;
 
 namespace XiaoZhi.Net.Server.Providers
 {
@@ -14,7 +12,7 @@ namespace XiaoZhi.Net.Server.Providers
         event Action<string> OnBeforeTokenGenerate;
         event Action<string, OutSegment> OnTokenGenerating;
         event Action<string, string> OnTokenGenerated;
-        Task ChatAsync( IEnumerable<Dialogue> dialogues,  Workflow<string> workflow, CancellationToken token);
-        Task ChatByStreamingAsync( IEnumerable<Dialogue> dialogues,  Workflow<string> workflow, CancellationToken token);
+        Task ChatAsync(IEnumerable<Dialogue> dialogues, Workflow<string> workflow, CancellationToken token);
+        Task ChatByStreamingAsync(IEnumerable<Dialogue> dialogues, Workflow<string> workflow, CancellationToken token);
     }
 }
