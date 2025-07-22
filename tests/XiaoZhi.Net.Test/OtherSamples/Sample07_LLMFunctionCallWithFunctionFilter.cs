f7771decb7f33d0462aec03143e802efd7e22a6e
mm7s <sun7key@gmail.com>
2025-07-22 18:38:26 +0800

    add function invocation filter testing

---
+++ tests/XiaoZhi.Net.Test/OtherSamples/Sample07_LLMFunctionCallWithFunctionFilter.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.Client;
+using OpenAI.Chat;
+using System.Collections.ObjectModel;
+using XiaoZhi.Net.Test.Filter;
+using XiaoZhi.Net.Test.Plugins;
+
+namespace XiaoZhi.Net.Test.OtherSamples
+{
+    internal class Sample07_LLMFunctionCallWithFunctionFilter
+    {
+        public static async Task Run()
+        {
+            await TestPromptSample();
+        }
+
+        static async Task TestPromptSample()
+        {
+            try
+            {
+                string endPoint = "https://open.bigmodel.cn/api/paas/v4/";
+                string apiKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY", EnvironmentVariableTarget.User)!;
+                string chatModel = "glm-4-flash";
+
+                IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+                IServiceCollection services = kernelBuilder.Services;
+                services.AddOpenAIChatCompletion(chatModel, new Uri(endPoint), apiKey, orgId: "Xiao Zhi");
+
+                services.AddSingleton<IFunctionInvocationFilter, PluginSelectionFilter>();
+
+                var kernel = kernelBuilder.Build();
+
+                kernel.ImportPluginFromType<TimePlugin>();
+                kernel.ImportPluginFromType<WeatherPlugin>();
+
+                //await InitMCP(kernel);
+                InitCustomFunctions(kernel);
+
+
+                var chatCompletionOptions = new OpenAIPromptExecutionSettings
+                {
+                    Temperature = 0.5f,
+                    MaxTokens = 80,
+                    ResponseFormat = ChatResponseFormat.CreateTextFormat(),
+                    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+                };
+
+                IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+                var clientResult = await chatCompletionService.GetChatMessageContentAsync("帮我打开taobao网站", chatCompletionOptions, kernel);
+                Console.WriteLine(clientResult.Content);
+
+                //if (kernel.Plugins.Remove(deviceMcpPlugins))
+                //{
+                //    Console.WriteLine("删除deviceMcpPlugins成功");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("删除deviceMcpPlugins失败");
+                //}
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+        }
+
+        static async Task InitMCP(Kernel kernel)
+        {
+            var (command, arguments) = GetCommandAndArguments();
+
+            var clientTransport = new StdioClientTransport(new()
+            {
+                Name = "Demo Server",
+                Command = command,
+                Arguments = arguments,
+            });
+
+            await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+
+            var tools = await mcpClient.ListToolsAsync();
+            foreach (var tool in tools)
+            {
+                Console.WriteLine($"Connected to server with tools: {tool.Name}");
+            }
+#pragma warning disable SKEXP0001
+            var functions = tools.Select(aiFunction => aiFunction.AsKernelFunction()).ToList();
+#pragma warning restore SKEXP0001
+
+            // kernel.Plugins.AddFromFunctions("Tools", functions);
+        }
+
+        static void InitCustomFunctions(Kernel kernel)
+        {
+            Func<string, Task<string>> openUrlMethod = (url) =>
+            {
+                Console.WriteLine("function调用：" + url);
+                return Task.FromResult("打开网站成功");
+            };
+
+            var parameters = new List<KernelParameterMetadata>
+                {
+                    new KernelParameterMetadata("url")
+                    {
+                        Description = "要打开的网址",
+                        IsRequired = true,
+                        ParameterType = typeof(string),
+                    }
+                };
+
+            IDictionary<string, object?> additionalMetadataDic = new Dictionary<string, object?>
+                {
+                    { "sessionId", "my-test-id" }
+                };
+
+            KernelFunctionFromMethodOptions openUrlFunctionOptions = new KernelFunctionFromMethodOptions
+            {
+                FunctionName = "open_url",
+                Description = "打开指定网址",
+                Parameters = parameters,
+                ReturnParameter = new KernelReturnParameterMetadata
+                {
+                    Description = "打开网站的结果",
+                    ParameterType = typeof(string),
+                },
+                AdditionalMetadata = new ReadOnlyDictionary<string, object?>(additionalMetadataDic)
+            };
+
+            var openUrlFunction = KernelFunctionFactory.CreateFromMethod(openUrlMethod, openUrlFunctionOptions);
+
+            var deviceMcpPlugins = kernel.ImportPluginFromFunctions("DeviceMcpFunctions", new List<KernelFunction> { openUrlFunction });
+        }
+
+        static (string command, string[] arguments) GetCommandAndArguments()
+        {
+            return ("dotnet", ["run", "--project", Path.Combine("C:\\Visual_D_Drive\\Projects\\Github\\csharp-sdk-0.3.0-preview.2\\samples\\QuickstartWeatherServer\\../QuickstartWeatherServer")]);
+        }
+    }
+}
---
+++ tests/XiaoZhi.Net.Test/Plugins/WeatherPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace XiaoZhi.Net.Test.Plugins
+{
+    public class WeatherPlugin
+    {
+        [KernelFunction, Description("根据城市名称获取当天的天气信息")]
+        public string GetWeather(string cityName) => cityName switch
+        {
+            "成都" => "61 and rainy",
+            "重庆" => "55 and cloudy",
+            "西安" => "80 and sunny",
+            "兰州" => "60 and rainy",
+            "西藏" => "50 and sunny",
+            "昆明" => "75 and sunny",
+            "贵阳" => "80 and sunny",
+            _ => "No information",
+        };
+    }
+}
